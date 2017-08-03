39d3e9358f4605e89c76c5dce7de4a985d0d8aa2
bloodes <stepan.barantsev@gmail.com>
2017-08-03 23:41:45 +0300

    Пока что все плохо

--- test_add_group.py
+++ test_add_group.py
@@ -9,8 +9,8 @@ import unittest, time, re
 
 class Untitled(unittest.TestCase):
     def setUp(self):
-        self.driver = webdriver.Firefox()
-        self.driver.implicitly_wait(30)
+        self.driver = webdriver.Chrome()
+        self.driver.implicitly_wait(5)
         self.base_url = "http://localhost/"
         self.verificationErrors = []
         self.accept_next_alert = True
@@ -18,22 +18,17 @@ class Untitled(unittest.TestCase):
     def test_untitled(self):
         driver = self.driver
         driver.get(self.base_url + "/addressbook/")
-        driver.find_element_by_name("user").clear()
         driver.find_element_by_name("user").send_keys("admin")
-        driver.find_element_by_css_selector("input[type=\"submit\"]").click()
+        driver.find_element_by_name("pass").send_keys("secret")
+        driver.find_element_by_css_selector("input[type='submit']").click()
         driver.find_element_by_link_text("groups").click()
-        driver.find_element_by_name("new").click()
-        driver.find_element_by_name("group_name").clear()
+        driver.find_element_by_css_selector('input[name="new"]').click()   # Не понимаю, что происходит, лол
         driver.find_element_by_name("group_name").send_keys("rrffr")
-        driver.find_element_by_name("group_header").clear()
         driver.find_element_by_name("group_header").send_keys("rfrfrf")
-        driver.find_element_by_name("group_footer").clear()
         driver.find_element_by_name("group_footer").send_keys("rfrfrfrfrfrfrf")
         driver.find_element_by_name("submit").click()
         driver.find_element_by_link_text("group page").click()
         driver.find_element_by_link_text("Logout").click()
-        driver.find_element_by_name("user").clear()
-        driver.find_element_by_name("user").send_keys("admin")
 
     def is_element_present(self, how, what):
         try: self.driver.find_element(by=how, value=what)
