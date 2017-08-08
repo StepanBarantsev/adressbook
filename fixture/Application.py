2cd46c988dda28353abba74b483937e72a93e49d
bloodes <stepan.barantsev@gmail.com>
2017-08-08 15:13:36 +0300

    Перенес open_home_page в session
    Все работает

--- fixture/Application.py
+++ fixture/Application.py
@@ -12,9 +12,5 @@ class Application:
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
 
-    def open_home_page(self):
-        driver = self.driver
-        driver.get("http://localhost/addressbook/")
-
     def destroy(self):
         self.driver.quit()
--- fixture/session.py
+++ fixture/session.py
@@ -5,14 +5,17 @@ class SessionHelper:
 
     def login(self, name, password):      # Принимает логин и пароль.
         driver = self.app.driver
-        self.app.open_home_page()
+        self.open_home_page()
         driver.find_element_by_name("user").clear()
         driver.find_element_by_name("pass").clear()
         driver.find_element_by_name("user").send_keys(name)
         driver.find_element_by_name("pass").send_keys(password)
         driver.find_element_by_css_selector("input[type='submit']").click()
 
-
     def logout(self):
         driver = self.app.driver
         driver.find_element_by_link_text("Logout").click()
+
+    def open_home_page(self):
+        driver = self.app.driver
+        driver.get("http://localhost/addressbook/")
