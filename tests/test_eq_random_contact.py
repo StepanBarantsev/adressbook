22b90eb91425266c021da6c816807c42627267f0
bloodes <stepan.barantsev@gmail.com>
2017-08-16 17:22:10 +0300

    Коммент написал)

--- tests/test_eq_random_contact.py
+++ tests/test_eq_random_contact.py
@@ -8,9 +8,9 @@ def merge_emails_like_on_homepage(contact):
                          [contact.email1, contact.email2, contact.email3])))
 
 
-def test_eq_of_random_contact(app):
+def test_eq_of_random_contact(app):   # Это сравнение с edit и homepage всех полей
     index = randrange(app.contact.count())
-    first_cont_from_homepage = app.contact.get_contact_list()[index]   # Можно без проблем и через индексы в цикле
+    first_cont_from_homepage = app.contact.get_contact_list()[index]
     first_cont_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
     assert first_cont_from_homepage.all_phones_from_homepage == merge_phones_like_on_homepage(first_cont_from_edit_page)
     assert first_cont_from_homepage.allemails == merge_emails_like_on_homepage(first_cont_from_edit_page)
